5902fafa56dcdab5a6553ce1533c716f73af6664
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-05-16 14:40:33 -0400

    made constants

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 from gpt4free import Provider
 
+PAGE_COUNT = 49
+SEARCH_TERM = "genetically modified organisms effects on the world"
+GET_SOUP = lambda url: BeautifulSoup(requests.get(url).html, "html.parser")
+
 def get_articles():
 
-    # obtains page ids
+    """
+    Stores the abstracts of scholarly articles into a text file with each line representing one abstract.
+    These abstracts can later be read and evaluated.
+    """
+
+    """ This part obtains the article id's to parse through, given a search term """
+
     article_ids = []
-    search_url = r"https://pubmed.ncbi.nlm.nih.gov/?term=genetically%20modified%20organisms%20effects%20on%20the%20world&page="
-    for page in range(1, 49):
-        html = requests.get(f"{search_url}{page}").text
-        soup = BeautifulSoup(html, "html.parser")
-        for link in soup.find_all("a", {"class": "docsum-title"}):
-            article_ids.append(link.attrs["data-article-id"])
-    
+    search_url = f"https://pubmed.ncbi.nlm.nih.gov/?term={SEARCH_TERM.replace(' ', '%20')}&page="
+
+    # in case of errors near the end
+    try:
+        for page in range(1, PAGE_COUNT):
+            soup = GET_SOUP(f"{search_url}{page}")
+            for link in soup.find_all("a", {"class": "docsum-title"}):
+                article_ids.append(link.attrs["data-article-id"])
+    except: pass        
+        
 
 def analyze_abstract(abstract):
 
